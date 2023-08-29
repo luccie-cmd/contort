5be923f356639e83ed64b2b89a12ae602a9266ec
luccie-cmd <hoedje201@gmail.com>
2023-08-29 21:48:59 +0200

    Remove printing of the types after lexing

--- src/main.c
+++ src/main.c
@@ -8,9 +8,6 @@ int main(){
     String file_data = string_slurp_file("./build/main.con");
     Lexer lexer = lexer_init(file_data);
     Token *toks = lexer_lex_file(&lexer, &len);
-    for(uint64_t i = 0; i < len; ++i){
-        printf("%d\n", toks[i].as.inst.type);
-    }
     free(toks);
     free(file_data.data);
     return 0;
